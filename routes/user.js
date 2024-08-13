a0c8982e69ed35181db41bcb3b559c500ca836f7
alltrackerx <alltrackerx@gmail.com>
2024-08-13 05:11:17 +0000

    added

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { requestOtp, verifyOtp, completeRegistration, login, getProfile } from '../controllers/user.js';
+import { protect } from '../middlewares/protect.js';
+
+const router = express.Router();
+
+router.post('/request-otp', requestOtp);
+router.post('/verify-otp', verifyOtp);
+router.post('/complete-registration', completeRegistration);
+router.post('/login', login);
+router.get('/protected-route', protect, getProfile);
+
+export default router;
