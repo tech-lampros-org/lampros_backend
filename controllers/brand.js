a1448dd7c2dc23768ce83b68d6415b5e5c755488
alltrackerx <alltrackerx@gmail.com>
2024-09-23 06:42:50 +0000

    up

---
+++ controllers/brand.js
@@ -0,0 +1,63 @@
+import Brand from '../models/brand.js';
+
+// Controller to handle adding a new brand
+export const addBrand = async (req, res) => {
+  try {
+    const { name, description, imageUrl } = req.body;
+
+    // Check if the brand already exists
+    const existingBrand = await Brand.findOne({ name });
+    if (existingBrand) {
+      return res.status(400).json({ message: 'Brand already exists.' });
+    }
+
+    // Create a new brand
+    const brand = new Brand({
+      name,
+      description,
+      imageUrl
+    });
+
+    // Save the brand to the database
+    await brand.save();
+
+    res.status(201).json({ message: 'Brand created successfully', brand });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to create brand', error: error.message });
+  }
+};
+
+
+// Controller for admin to approve a brand
+export const approveBrand = async (req, res) => {
+    try {
+      const { brandId } = req.params;
+  
+      // Find the brand by ID
+      const brand = await Brand.findById(brandId);
+      if (!brand) {
+        return res.status(404).json({ message: 'Brand not found' });
+      }
+  
+      // Update approval status
+      brand.adminApproved = true;
+      await brand.save();
+  
+      res.status(200).json({ message: 'Brand approved successfully', brand });
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to approve brand', error: error.message });
+    }
+  };
+
+  
+  export const listAllBrands = async (req, res) => {
+    try {
+      // Fetch all brands
+      const brands = await Brand.find();
+  
+      // Send the brands as a response
+      res.status(200).json(brands);
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to retrieve brands', error: error.message });
+    }
+  };
