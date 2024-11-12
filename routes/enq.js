31e4d7d8810779ebd46cf808963040710724b86b
alltrackerx <alltrackerx@gmail.com>
2024-11-12 14:47:34 +0000

    up

--- routes/enq.js
+++ routes/enq.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createEnquiry } from '../controllers/enq.js';
+import { createEnquiry,getAllEnquiries } from '../controllers/enq.js';
 
 const router = express.Router();
 
