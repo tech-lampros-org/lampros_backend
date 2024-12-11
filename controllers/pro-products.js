c91fb5ab66a39281fea1ca84958e9465f04054b0
alltrackerx <alltrackerx@gmail.com>
2024-12-11 14:35:03 +0000

    ok

--- controllers/pro-products.js
+++ controllers/pro-products.js
@@ -285,6 +285,16 @@ export const filterProducts = async (req, res) => {
       query['seller.phoneNumber'] = { $in: PhoneNumber };
     }
 
+    if (isValid(brand)) {
+      const brandNames = brand.split(',');
+
+      // Fetch matching brand IDs from the Brand collection
+      const brandDocs = await Brand.find({ name: { $in: brandNames } }, '_id');
+      const brandIds = brandDocs.map((doc) => doc._id);
+
+      query.brand = { $in: brandIds };
+    }
+    
     if (isValid(location)) {
       query['seller.location'] = location;
     }
